3a722bc6bbcb21601811a5283f2d78fedc557a81
Duman Marlambekov <74466653+Dumanbruh@users.noreply.github.com>
2024-10-08 16:28:42 +0500

    Added typescript and map library (#3)
    * Initialize project using Create React App
    
    * added dependencies and cleaned proj
    
    * Added typescript and some components
    
    * chore: Update dependencies and added map component
    
    * chore: Update manifest.json and add Circle component
    
    - Updated the "short_name" property in manifest.json to "AirDEX" for better branding.
    - Added a new Circle component in src/components/circle.tsx for rendering circles on the map.
    
    * feat: Add Dockerfile for containerization
    
    ---------
    
    Co-authored-by: Duman <201605@astanait.edu.kz>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}', './src/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: '600px ',
+        md: '728px',
+        lg: '984px',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        white: "#FFFFFF",
+        black: "#353535",
+        green: "#3C6E71",
+        gray: "#D9D9D9",
+        blue: "#284B63",
+      }
+    },
   },
   plugins: [],
 }
