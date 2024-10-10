392ada8c56d59a385465bddb92006b967bf71486
Duman <201605@astanait.edu.kz>
2024-10-10 12:46:43 +0500

    Refactor AppLayout to fetch air data and display it

--- src/common/AppLayout.tsx
+++ src/common/AppLayout.tsx
@@ -1,7 +1,13 @@
 import Header from '../components/ui/header'
 import CustomMap from '../pages/home/component/custom-map'
+import { useFetchAirData } from '../services/air-sensor';
 
 const AppLayout = () => {
+  const {
+    data,
+    isLoading
+  } = useFetchAirData();
+
   return (
     <div className={"container flex flex-col"}>
       <Header />
---
+++ src/services/air-sensor.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "react-query";
+import { apiClient } from "../common/apiClient"
+import { IAirSensorSignal } from "../interfaces/IAirSensorSignal"
+
+const QUERY_KEY = 'air-quality';
+
+const fetchData = async (): Promise<IAirSensorSignal> => {
+    const { data } = await apiClient.airSensor.findAll();
+    return data;
+}
+
+export const useFetchAirData = () => {
+    return useQuery(QUERY_KEY, () => fetchData());
+}
