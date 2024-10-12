a18afa1494405a70ab6b2b91e40d41c626198658
DumanBruh <dmarlambekov@gmail.com>
2024-10-12 21:31:15 +0500

    Refactor API client to use consistent URL format

--- src/common/apiClient.ts
+++ src/common/apiClient.ts
@@ -12,12 +12,12 @@ const baseURL = process.env.REACT_APP_BACKEND_URL;
 export const apiClient = {
   airSensor: {
     findAll: async (): Promise<AxiosResponse<IAirSensorSignal[]>> => {
-      return await axios.get(`${baseURL}air-signals`);
+      return await axios.get(`${baseURL}/air-signals`);
     },
     save: async (
       data: IAirSensorSignalAcceptRequest
     ): Promise<AxiosResponse<IAirSensorSignal>> => {
-      return await axios.post(`${baseURL}air-signals`, data);
+      return await axios.post(`${baseURL}/air-signals`, data);
     },
   },
 };
