230e573565cd20a988e4a90fbdc27a77fa3b97a4
DumanBruh <dmarlambekov@gmail.com>
2024-10-12 17:36:24 +0500

    Refactor map component to handle camera changes and initial position

--- src/pages/home/component/custom-map.tsx
+++ src/pages/home/component/custom-map.tsx
@@ -1,4 +1,4 @@
-import { Map, Marker, } from '@vis.gl/react-google-maps'
+import { AdvancedMarker, Map, MapCameraChangedEvent, MapCameraProps, Marker } from '@vis.gl/react-google-maps'
 import React from 'react'
 import { IAirSensorSignal } from '../../../interfaces/IAirSensorSignal';
 import SensorCircle from './sensor-circle';
@@ -7,27 +7,39 @@ interface CustomMapProps {
     airSensorData: IAirSensorSignal[];
 }
 
+const INITIAL_CAMERA = {
+    center: {
+        lat: localStorage.getItem("currentPosition") ? JSON.parse(localStorage.getItem("currentPosition") || "").lat : 0,
+        lng: localStorage.getItem("currentPosition") ? JSON.parse(localStorage.getItem("currentPosition") || "").lng : 0,
+    },
+    zoom: 14
+};
+
 const CustomMap = ({ airSensorData }: CustomMapProps) => {
+
+    const [cameraProps, setCameraProps] =
+        React.useState<MapCameraProps>(INITIAL_CAMERA);
+    const handleCameraChange = React.useCallback((ev: MapCameraChangedEvent) =>
+        setCameraProps(ev.detail), []
+    );
+
     return (
         <Map
             style={{ width: '100%', height: "500px" }}
             gestureHandling={'greedy'}
             disableDefaultUI={true}
-            defaultCenter={{
-                lat: localStorage.getItem("currentPosition") ? JSON.parse(localStorage.getItem("currentPosition") || "").lat : 0,
-                lng: localStorage.getItem("currentPosition") ? JSON.parse(localStorage.getItem("currentPosition") || "").lng : 0,
-            }}
-            defaultZoom={15}
+            {...cameraProps}
+            onCameraChanged={handleCameraChange}
         >
             {airSensorData?.filter(v => v.lat !== null && v.lon !== null).map((sensor, index) => (
                 <SensorCircle sensor={sensor} key={index} />
             ))}
             {localStorage.getItem("currentPosition") && (
                 <Marker
-                    key={"current"}
+                    key="currentPosition"
                     position={{
-                        lat: localStorage.getItem("currentPosition") ? JSON.parse(localStorage.getItem("currentPosition") || "").lat : 0,
-                        lng: localStorage.getItem("currentPosition") ? JSON.parse(localStorage.getItem("currentPosition") || "").lng : 0,
+                        lat: JSON.parse(localStorage.getItem("currentPosition") || "").lat,
+                        lng: JSON.parse(localStorage.getItem("currentPosition") || "").lng
                     }}
                 />
             )}
