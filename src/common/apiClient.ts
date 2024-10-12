5bdc4701e331f757fff29c126d2bbe1d4edeba60
Duman Marlambekov <74466653+Dumanbruh@users.noreply.github.com>
2024-10-12 21:22:44 +0500

    Hot fix (#10)
    * Refactor map component to display air sensor data and show info window on circle click
    
    * Refactor air sensor signal interface and custom map component
    
    * Refactor geolocation handling in AppLayout and CustomMap components
    
    * Refactor getFullDateWithTime function to format date and time
    
    * Refactor CSS for Google Maps info window and base styles
    
    * Refactor page title and description in index.html
    
    * Refactor map component to handle camera changes and initial position
    
    * Refactor getFullDateWithTime function to use date-fns library for date and time formatting
    
    * Refactor mapUtils.tsx to use date-fns library for date and time formatting
    Refactor routes.tsx to import AppLayout from the correct location
    Move AppLayout.tsx to the pages/home directory
    Update CustomMap.tsx to handle camera changes and display info window
    Remove SensorCircle.tsx from the project
    
    * Refactor import statement in custom-map.tsx to remove unused RenderingType import
    
    * Refactor apiClient to use dynamic base URL
    
    ---------
    
    Co-authored-by: Duman <201605@astanait.edu.kz>

--- src/common/apiClient.ts
+++ src/common/apiClient.ts
@@ -4,19 +4,21 @@ import {
   IAirSensorSignalAcceptRequest,
 } from "../interfaces/IAirSensorSignal";
 
-axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
 axios.defaults.timeout = 300000;
 axios.defaults.withCredentials = false;
+
+const baseURL =
+  process.env.REACT_APP_API_URL || "http://localhost:8080/api/v1/";
 
 export const apiClient = {
   airSensor: {
     findAll: async (): Promise<AxiosResponse<IAirSensorSignal[]>> => {
-      return await axios.get("/air-signals");
+      return await axios.get(`${baseURL}/air-signals`);
     },
     save: async (
       data: IAirSensorSignalAcceptRequest
     ): Promise<AxiosResponse<IAirSensorSignal>> => {
-      return await axios.post("/air-signals", data);
+      return await axios.post(`${baseURL}/air-signals`, data);
     },
   },
 };
