05c52c929a35c5a575ed460a4a15a14026d6df3b
Duman <201605@astanait.edu.kz>
2024-10-18 11:29:31 +0500

    Fix stroke attribute names in spinner component

--- src/pages/home/AppLayout.tsx
+++ src/pages/home/AppLayout.tsx
@@ -6,24 +6,24 @@ import { useFetchSectors } from '../../services/air-sensor';
 const AppLayout = () => {
   const { data } = useFetchSectors();
 
-  React.useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          localStorage.setItem("currentPosition", JSON.stringify({ lat: latitude, lng: longitude }));
-        },
-        (error) => {
-          console.error("Error getting location:", error);
-          localStorage.clear();
-        }
-      );
-    }
-    else {
-      console.error("Geolocation is not supported by this browser.");
-      localStorage.clear();
-    }
-  }, []);
+  // React.useEffect(() => {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(
+  //       (position) => {
+  //         const { latitude, longitude } = position.coords;
+  //         localStorage.setItem("currentPosition", JSON.stringify({ lat: latitude, lng: longitude }));
+  //       },
+  //       (error) => {
+  //         console.error("Error getting location:", error);
+  //         localStorage.clear();
+  //       }
+  //     );
+  //   }
+  //   else {
+  //     console.error("Geolocation is not supported by this browser.");
+  //     localStorage.clear();
+  //   }
+  // }, []);
 
   return (
     <div className={"container flex flex-col"}>
