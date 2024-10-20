52d43cf6e5f637282cbbeaa1c4ab2bd3a19415af
DumanBruh <dmarlambekov@gmail.com>
2024-10-21 00:08:28 +0500

    Font change

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header: React.FC = () => {
     return (
-        <div className={"flex items-center px-2 py-4 border-b-[1px] border-black mb-5"}>
-            <h1 className={"text-2xl font-bold text-blue"}>AIRDEX</h1>
+        <div className={"flex items-center px-2 py-8 border-b-[1px] border-black mb-5"}>
+            <h1 className={"text-xl leading-5 font-bold text-black"}>Airdex Monitoring</h1>
         </div>
     )
 }
